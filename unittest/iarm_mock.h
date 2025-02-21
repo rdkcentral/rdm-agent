cc1fc6b57cc3b8f1d3509d62e9b79b2c6a688129
Vismalskumar0 <ajvismal@yahoo.com>
2025-02-21 12:07:34 +0530

    Create iarm_mock.h

---
+++ unittest/iarm_mock.h
@@ -0,0 +1,16 @@
+// iarm_mock.h
+#ifndef IARM_MOCK_H  // Include guard
+#define IARM_MOCK_H
+
+#include <gmock/gmock.h>
+#include "./iarm/libIBus.h"
+class MockIARM {
+public:
+    MOCK_METHOD(IARM_Result_t, IARM_Bus_Init, (const char*), ());
+    MOCK_METHOD(IARM_Result_t, IARM_Bus_Connect, (), ());
+    MOCK_METHOD0(IARM_Bus_Disconnect, IARM_Result_t());
+    MOCK_METHOD0(IARM_Bus_Term, IARM_Result_t());
+    MOCK_METHOD(IARM_Result_t, IARM_Bus_BroadcastEvent, (const char*, IARM_EventId_t, void*, size_t), ());
+};
+
+#endif // IARM_MOCK_H
