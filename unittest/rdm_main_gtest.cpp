eca687a813a42eb78cc1af868527710b034fc650
Vismalskumar0 <ajvismal@yahoo.com>
2025-04-01 11:14:02 +0530

    Update rdm_main_gtest.cpp

--- unittest/rdm_main_gtest.cpp
+++ unittest/rdm_main_gtest.cpp
@@ -1,123 +1,62 @@
+// test_rdm_main.cpp
+
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include "rdmMgr.h"
-#include "rdm_types.h"
 #include "rdm.h"
-#include "rdm_utils.h"
-#include "rdm_jsonquery.h"
-#include "rdm_download.h"
-#include "rdm_downloadutils.h"
-#include "rdm_curldownload.h"
-#include "rdm_rbus.h"
-#include "rdm_usbinstall.h"
+#include "rdmMgr.h"
+#include "mock_rdm_rbus.h"
 
-using namespace testing;
+MockRdmRbus* g_mockRdmRbus = nullptr;
 
-// Mock classes for external dependencies
-class MockRdm : public ::testing::Test {
+class RDMTest : public ::testing::Test {
 protected:
-    void SetUp() override {
-        // Initialize mocks and set default behaviors
-        memset(&mockHandle, 0, sizeof(RDMHandle));
-        mockHandle.pApp_det = new RDMAPPDetails();
+    RDMHandle* prdmHandle;
+
+    virtual void SetUp() {
+        prdmHandle = (RDMHandle*)malloc(sizeof(RDMHandle));
+        memset(prdmHandle, 0, sizeof(RDMHandle));
+        g_mockRdmRbus = new MockRdmRbus();
     }
 
-    void TearDown() override {
-        if (mockHandle.pApp_det) {
-            delete mockHandle.pApp_det;
+    virtual void TearDown() {
+        if (prdmHandle) {
+            free(prdmHandle);
         }
+        delete g_mockRdmRbus;
+        g_mockRdmRbus = nullptr;
     }
-
-    RDMHandle mockHandle;
 };
 
-// Mock functions
-MOCK_FUNCTION3(rdmRbusInit, INT32(RDMRbusHandle**, const CHAR*, const CHAR*));
-MOCK_FUNCTION1(rdmRbusUnInit, VOID(RDMRbusHandle*));
-MOCK_FUNCTION2(rdmJSONGetLen, INT32(const char*, INT32*));
-MOCK_FUNCTION2(rdmJSONGetAppDetID, INT32(INT32, RDMAPPDetails*));
-MOCK_FUNCTION3(rdmUpdateAppDetails, INT32(RDMHandle*, RDMAPPDetails*, INT32));
-MOCK_FUNCTION2(rdmDownloadApp, INT32(RDMAPPDetails*, INT32*));
-MOCK_FUNCTION3(rdmRbusGetRfc, INT32(RDMRbusHandle*, const char*, INT32*));
-MOCK_FUNCTION3(rdmUSBInstall, INT32(RDMHandle*, RDMAPPDetails*, const char*));
-
-// Test cases
-TEST_F(MockRdm, TestRdmInitSuccess) {
-    // Arrange
-    EXPECT_CALL(*this, rdmRbusInit(_, _, _)).WillOnce(Return(RDM_SUCCESS));
-
-    // Act
-    INT32 ret = rdmInit(&mockHandle);
-
-    // Assert
-    EXPECT_EQ(ret, RDM_SUCCESS);
+TEST_F(RDMTest, rdmInit_NullHandle) {
+    EXPECT_EQ(rdmInit(NULL), RDM_FAILURE);
 }
 
-TEST_F(MockRdm, TestRdmInitFailure) {
-    // Arrange
-    EXPECT_CALL(*this, rdmRbusInit(_, _, _)).WillOnce(Return(RDM_FAILURE));
+TEST_F(RDMTest, rdmInit_Success) {
+    EXPECT_CALL(*g_mockRdmRbus, rdmRbusInit(testing::_, testing::_))
+        .WillOnce(testing::Return(RDM_SUCCESS));
 
-    // Act
-    INT32 ret = rdmInit(&mockHandle);
-
-    // Assert
-    EXPECT_EQ(ret, RDM_FAILURE);
+    EXPECT_EQ(rdmInit(prdmHandle), RDM_SUCCESS);
+    EXPECT_NE(prdmHandle->pApp_det, nullptr);
+    free(prdmHandle->pApp_det);
 }
 
-TEST_F(MockRdm, TestRdmUnInit) {
-    // Arrange
-    EXPECT_CALL(*this, rdmRbusUnInit(_)).Times(1);
+TEST_F(RDMTest, rdmInit_Failure) {
+    EXPECT_CALL(*g_mockRdmRbus, rdmRbusInit(testing::_, testing::_))
+        .WillOnce(testing::Return(RDM_FAILURE));
 
-    // Act
-    rdmUnInit(&mockHandle);
-
-    // Assert
-    // Verify that rdmRbusUnInit is called
+    EXPECT_EQ(rdmInit(prdmHandle), RDM_FAILURE);
+    free(prdmHandle->pApp_det);
 }
 
-TEST_F(MockRdm, TestDownloadAllAppsSuccess) {
-    // Arrange
-    EXPECT_CALL(*this, rdmJSONGetLen(_, _)).WillOnce(DoAll(SetArgPointee<1>(3), Return(RDM_SUCCESS)));
-    EXPECT_CALL(*this, rdmJSONGetAppDetID(_, _)).Times(3).WillRepeatedly(Return(RDM_SUCCESS));
-    EXPECT_CALL(*this, rdmUpdateAppDetails(_, _, _)).Times(3).WillRepeatedly(Return(RDM_SUCCESS));
-    EXPECT_CALL(*this, rdmDownloadApp(_, _)).Times(3).WillRepeatedly(Return(RDM_SUCCESS));
+TEST_F(RDMTest, rdmUnInit) {
+    prdmHandle->pApp_det = (RDMAPPDetails*)malloc(sizeof(RDMAPPDetails));
+    EXPECT_CALL(*g_mockRdmRbus, rdmRbusUnInit(testing::_))
+        .Times(1);
 
-    // Act
-    INT32 ret = main(1, nullptr); // Simulate "rdm" with no arguments
-
-    // Assert
-    EXPECT_EQ(ret, RDM_SUCCESS);
+    rdmUnInit(prdmHandle);
+    EXPECT_EQ(prdmHandle->pApp_det, nullptr);
 }
 
-TEST_F(MockRdm, TestDownloadSingleAppSuccess) {
-    // Arrange
-    char* argv[] = {"rdm", "-a", "test_app"};
-    EXPECT_CALL(*this, rdmJSONGetAppDetName(_, _)).WillOnce(Return(RDM_SUCCESS));
-    EXPECT_CALL(*this, rdmUpdateAppDetails(_, _, _)).WillOnce(Return(RDM_SUCCESS));
-    EXPECT_CALL(*this, rdmDownloadApp(_, _)).WillOnce(Return(RDM_SUCCESS));
-
-    // Act
-    INT32 ret = main(3, argv);
-
-    // Assert
-    EXPECT_EQ(ret, RDM_SUCCESS);
-}
-
-TEST_F(MockRdm, TestUsbInstallSuccess) {
-    // Arrange
-    char* argv[] = {"rdm", "-u", "/path/to/usb"};
-    EXPECT_CALL(*this, rdmUSBInstall(_, _, _)).WillOnce(Return(RDM_SUCCESS));
-
-    // Act
-    INT32 ret = main(3, argv);
-
-    // Assert
-    EXPECT_EQ(ret, RDM_SUCCESS);
-}
-
-// Add more tests to cover edge cases, failures, and specific functionalities as needed
-
-// Main entry point for tests
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
