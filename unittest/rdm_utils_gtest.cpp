c0724112f7cdb43cd0ad4486efe1b84870c55766
Aravindan NC <35158113+AravindanNC@users.noreply.github.com>
2025-04-15 09:41:38 -0400

    Update rdm_utils_gtest.cpp

--- unittest/rdm_utils_gtest.cpp
+++ unittest/rdm_utils_gtest.cpp
@@ -4,6 +4,7 @@
 extern "C" {
 #include "unittest/mocks/libIBus.h"
 #include "unittest/mocks/rdmMgr.h"
+#include "unittest/mocks/mock_rdm_utils.h"
 #include "rdm_download.h"
 #include "rdm_downloadutils.h"
 #include <dirent.h>
