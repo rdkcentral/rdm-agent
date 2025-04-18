ef38b8945d849d3f190a726d85f1b7eb56c989b4
Aravindan NC <nc.aravindan@gmail.com>
2025-04-18 14:49:38 +0000

    debug

--- unittest/mocks/mock_rdm_rbus.h
+++ unittest/mocks/mock_rdm_rbus.h
@@ -8,7 +8,7 @@ class MockRdmRbus {
 public:
     MOCK_METHOD(rbusValueType_t, rbusValue_GetType, (void*), ());
     MOCK_METHOD(INT32, rbus_get, (void*, INT8*, void*), ());
-    MOCK_METHOD(INT8*, rbusValue_ToString, (void*, void*, int), ());
+    MOCK_METHOD(const INT8*, rbusValue_ToString, (void*, void*, int), ());
     MOCK_METHOD(bool, rbusValue_GetBoolean, (void*), ());
     MOCK_METHOD(void, rbusValue_Release, (void*), ());
 };
