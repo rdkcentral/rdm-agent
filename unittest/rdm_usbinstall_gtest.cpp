216ae9e06819edecad88cea6a435067bbb959323
Vismal S Kumar <ajvismal@yahoo.com>
2025-04-23 16:48:17 +0530

    Update rdm_usbinstall_gtest.cpp

--- unittest/rdm_usbinstall_gtest.cpp
+++ unittest/rdm_usbinstall_gtest.cpp
@@ -12,53 +12,89 @@ extern "C" {
 #define GTEST_REPORT_FILEPATH_SIZE 256
 #define MAX_BUFF_SIZE 256  // Define MAX_BUFF_SIZE if not defined elsewhere
 
-// Mock declarations for functions being tested
-MOCK_FUNCTION5(findPFileAll, int(const char*, const char*, char*[], int*, int));
-MOCK_FUNCTION2(getPartChar, char*(const char*, char));
-MOCK_FUNCTION2(getPartStr, char*(const char*, const char*));
-MOCK_FUNCTION2(rdmJSONGetAppDetName, int(const char*, char*));
-MOCK_FUNCTION3(rdmUpdateAppDetails, int(RDMHandle*, const char*, int));
-MOCK_FUNCTION1(rdmPrintAppDetails, void(const char*));
-MOCK_FUNCTION2(rdmDownloadApp, int(const char*, const char*));
+class MockRdmUsbInstall {
+public:
+    MOCK_METHOD(int, findPFileAll, (const char*, const char*, char*[], int*, int));
+    MOCK_METHOD(char*, getPartChar, (const char*, char));
+    MOCK_METHOD(char*, getPartStr, (const char*, const char*));
+    MOCK_METHOD(int, rdmJSONGetAppDetName, (const char*, char*));
+    MOCK_METHOD(int, rdmUpdateAppDetails, (RDMHandle*, const char*, int));
+    MOCK_METHOD(void, rdmPrintAppDetails, (const char*));
+    MOCK_METHOD(int, rdmDownloadApp, (const char*, const char*));
+};
 
-// Test fixture class
-class RdmUSBInstallTest : public ::testing::Test {
+// Global mock object
+MockRdmUsbInstall* global_mock = nullptr;
+
+// Mock function definitions
+extern "C" {
+    int findPFileAll(const char* usbPath, const char* ext, char* apps[], int* numApps, int maxApps) {
+        return global_mock->findPFileAll(usbPath, ext, apps, numApps, maxApps);
+    }
+
+    char* getPartChar(const char* str, char delimiter) {
+        return global_mock->getPartChar(str, delimiter);
+    }
+
+    char* getPartStr(const char* str, const char* delimiter) {
+        return global_mock->getPartStr(str, delimiter);
+    }
+
+    int rdmJSONGetAppDetName(const char* jsonFile, char* appName) {
+        return global_mock->rdmJSONGetAppDetName(jsonFile, appName);
+    }
+
+    int rdmUpdateAppDetails(RDMHandle* handle, const char* appName, int flags) {
+        return global_mock->rdmUpdateAppDetails(handle, appName, flags);
+    }
+
+    void rdmPrintAppDetails(const char* details) {
+        global_mock->rdmPrintAppDetails(details);
+    }
+
+    int rdmDownloadApp(const char* appName, const char* destination) {
+        return global_mock->rdmDownloadApp(appName, destination);
+    }
+}
+
+class RdmUsbInstallTest : public ::testing::Test {
 protected:
     RDMHandle rdmHandle;
     RDMAPPDetails appDetails;
     char usbPath[MAX_BUFF_SIZE];
 
     void SetUp() override {
+        global_mock = new MockRdmUsbInstall();
         memset(&rdmHandle, 0, sizeof(rdmHandle));
         memset(&appDetails, 0, sizeof(appDetails));
         snprintf(usbPath, MAX_BUFF_SIZE, "/mock/usb_path");
     }
 
     void TearDown() override {
-        // Cleanup code if needed
+        delete global_mock;
+        global_mock = nullptr;
     }
 };
 
-// Test rdmUSBInstall with valid inputs
-TEST_F(RdmUSBInstallTest, RdmUSBInstall_Success) {
+TEST_F(RdmUsbInstallTest, RdmUSBInstall_Success) {
     char* mockUsbApps[RDM_MAX_USB_APP] = { strdup("app1_signed.tar"), strdup("app2_signed.tar") };
     int mockNumApps = 2;
 
-    EXPECT_CALL(findPFileAll, testing::_, testing::_, testing::_, testing::_, testing::_)
+    EXPECT_CALL(*global_mock, findPFileAll(testing::_, testing::_, testing::_, testing::_, testing::_))
         .WillOnce(::testing::DoAll(::testing::SetArrayArgument<2>(mockUsbApps, mockUsbApps + mockNumApps),
                                    ::testing::SetArgPointee<3>(mockNumApps)));
 
-    EXPECT_CALL(getPartChar, testing::_, testing::_)
+    EXPECT_CALL(*global_mock, getPartChar(testing::_, testing::_))
         .WillRepeatedly(::testing::Return(strdup("app1_signed.tar")));
-    EXPECT_CALL(getPartStr, testing::_, testing::_)
+    EXPECT_CALL(*global_mock, getPartStr(testing::_, testing::_))
         .WillRepeatedly(::testing::Return(strdup("signed")));
 
-    EXPECT_CALL(rdmJSONGetAppDetName, testing::_, testing::_)
+    EXPECT_CALL(*global_mock, rdmJSONGetAppDetName(testing::_, testing::_))
         .WillRepeatedly(::testing::Return(0));
-    EXPECT_CALL(rdmUpdateAppDetails, testing::_, testing::_)
+    EXPECT_CALL(*global_mock, rdmUpdateAppDetails(testing::_, testing::_))
         .WillOnce(::testing::Return(0));
-    EXPECT_CALL(rdmPrintAppDetails, testing::_);
-    EXPECT_CALL(rdmDownloadApp, testing::_, testing::_)
+    EXPECT_CALL(*global_mock, rdmPrintAppDetails(testing::_));
+    EXPECT_CALL(*global_mock, rdmDownloadApp(testing::_, testing::_))
         .WillRepeatedly(::testing::Return(0));
 
     int result = rdmUSBInstall(&rdmHandle, &appDetails, usbPath);
@@ -71,11 +107,10 @@ TEST_F(RdmUSBInstallTest, RdmUSBInstall_Success) {
     }
 }
 
-// Test rdmUSBInstall with no apps found
-TEST_F(RdmUSBInstallTest, RdmUSBInstall_NoAppsFound) {
+TEST_F(RdmUsbInstallTest, RdmUSBInstall_NoAppsFound) {
     int mockNumApps = 0;
 
-    EXPECT_CALL(findPFileAll, testing::_, testing::_, testing::_, testing::_, testing::_)
+    EXPECT_CALL(*global_mock, findPFileAll(testing::_, testing::_, testing::_, testing::_, testing::_))
         .WillOnce(::testing::SetArgPointee<3>(mockNumApps));
 
     int result = rdmUSBInstall(&rdmHandle, &appDetails, usbPath);
@@ -83,21 +118,20 @@ TEST_F(RdmUSBInstallTest, RdmUSBInstall_NoAppsFound) {
     EXPECT_EQ(result, RDM_SUCCESS);
 }
 
-// Test rdmUSBInstall with JSON parsing failure
-TEST_F(RdmUSBInstallTest, RdmUSBInstall_JSONParsingFailure) {
+TEST_F(RdmUsbInstallTest, RdmUSBInstall_JSONParsingFailure) {
     char* mockUsbApps[RDM_MAX_USB_APP] = { strdup("app1_signed.tar") };
     int mockNumApps = 1;
 
-    EXPECT_CALL(findPFileAll, testing::_, testing::_, testing::_, testing::_, testing::_)
+    EXPECT_CALL(*global_mock, findPFileAll(testing::_, testing::_, testing::_, testing::_, testing::_))
         .WillOnce(::testing::DoAll(::testing::SetArrayArgument<2>(mockUsbApps, mockUsbApps + mockNumApps),
                                    ::testing::SetArgPointee<3>(mockNumApps)));
 
-    EXPECT_CALL(getPartChar, testing::_, testing::_)
+    EXPECT_CALL(*global_mock, getPartChar(testing::_, testing::_))
         .WillRepeatedly(::testing::Return(strdup("app1_signed.tar")));
-    EXPECT_CALL(getPartStr, testing::_, testing::_)
+    EXPECT_CALL(*global_mock, getPartStr(testing::_, testing::_))
         .WillRepeatedly(::testing::Return(strdup("signed")));
 
-    EXPECT_CALL(rdmJSONGetAppDetName, testing::_, testing::_)
+    EXPECT_CALL(*global_mock, rdmJSONGetAppDetName(testing::_, testing::_))
         .WillOnce(::testing::Return(1)); // Simulate failure
 
     int result = rdmUSBInstall(&rdmHandle, &appDetails, usbPath);
