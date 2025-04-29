6206aaca95379e2ea3b7a2ece47e44b3dcec0eff
Vismal S Kumar <ajvismal@yahoo.com>
2025-04-30 02:21:38 +0530

    Update mock_rdm_utils.h

--- unittest/mocks/mock_rdm_utils.h
+++ unittest/mocks/mock_rdm_utils.h
@@ -35,6 +35,8 @@ public:
     MOCK_METHOD1(emptyFolder, INT32(const CHAR*));
     MOCK_METHOD1(getFreeSpace, UINT32(const CHAR*));
     MOCK_METHOD1(createDir, INT32(const CHAR*));
+    MOCK_METHOD(int, rdmJSONQuery, (const char*, const char*, char*), ());
+    MOCK_METHOD(int, rdmDwnlValidation, (const char*), ());
     MOCK_METHOD(void*, doCurlInit, (), ());
     MOCK_METHOD(INT32, doHttpFileDownload, (void*, FileDwnl_t*, MtlsAuth_t*, unsigned int, char*, int*), ());
     MOCK_METHOD(void, doStopDownload, (void *), ());
