c28bfdbe440746c47a4383345dc57b14da5f7810
Saranya <saranya_elango@comcast.com>
2025-05-16 10:17:43 +0000

    RDKE-3516 : Fix coveriyty issue

--- rdm_main.c
+++ rdm_main.c
@@ -305,11 +305,13 @@ int main(int argc, char* argv[])
 	    RDMInfo("Install App from custom path: %s\n", app_name);
 	    char *ver = strchr(app_name, ':');
 	    if (ver != NULL) {
-		    strcpy(pApp_det->pkg_ver, ver + 1);
+		    strncpy(pApp_det->pkg_ver, ver + 1, sizeof(pApp_det->pkg_ver) - 1);
+		    pApp_det->pkg_ver[sizeof(pApp_det->pkg_ver) - 1] = '\0';
 	    }
 	    sscanf(app_name, "%[^:]", result);
-	    strcpy(pApp_det->app_name, result);
-	    sprintf(pApp_det->pkg_name, "%s_%s-signed.tar", result, pApp_det->pkg_ver);
+	    strncpy(pApp_det->app_name, result, sizeof(pApp_det->app_name) - 1);
+            pApp_det->app_name[sizeof(pApp_det->app_name) - 1] = '\0';
+	    snprintf(pApp_det->pkg_name, sizeof(pApp_det->pkg_name), "%s_%s-signed.tar", result, pApp_det->pkg_ver);
 	    RDMInfo("pkg_name_signed = %s\n", pApp_det->pkg_name);
 	    pApp_det->is_versioned_app = 1;
 
