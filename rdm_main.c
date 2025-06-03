b086dfe4ebccd229e6d1423d82464656a3d40e2b
Aravindan NC <35158113+AravindanNC@users.noreply.github.com>
2025-06-03 09:23:49 -0400

    Update rdm_main.c

--- rdm_main.c
+++ rdm_main.c
@@ -330,7 +330,7 @@ int main(int argc, char* argv[])
     }
 
 error1:
-    rdmDwnlCleanUp(pApp_det);
+    rdmDwnlUnInstallApp(pApp_det->app_dwnl_path, pApp_det->app_home);
 
     if(download_status == 0) {
         RDMInfo("App download success, sending status as %d\n", download_status);
