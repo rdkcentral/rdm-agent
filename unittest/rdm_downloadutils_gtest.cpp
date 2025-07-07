09d1c0b231a0392f2dfa8a34536b741dd1b2eb0d
Vismal S Kumar <ajvismal@yahoo.com>
2025-07-07 16:12:13 +0530

    Update rdm_downloadutils_gtest.cpp

--- unittest/rdm_downloadutils_gtest.cpp
+++ unittest/rdm_downloadutils_gtest.cpp
@@ -33,7 +33,7 @@ extern "C" {
 }
 
 #define GTEST_DEFAULT_RESULT_FILEPATH "/tmp/Gtest_Report/"
-#define GTEST_DEFAULT_RESULT_FILENAME "rdmdownload_gtest_report.json"
+#define GTEST_DEFAULT_RESULT_FILENAME "rdm_downloadtils_gtest_report.json"
 #define GTEST_REPORT_FILEPATH_SIZE 256
 using ::testing::_;
 using ::testing::Return;
