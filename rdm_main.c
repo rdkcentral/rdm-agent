b319ea59620133d0ee65110445eabddd1a0f9084
Saranya <saranya_elango@comcast.com>
2025-10-31 11:32:56 +0000

    Code Formatting

--- rdm_main.c
+++ rdm_main.c
@@ -187,9 +187,9 @@ int main(int argc, char* argv[])
                     is_oss = 1;
                     break;
 		case 'v':
-		    download_versionedapp = 1;
-		    app_name = optarg;
-		    break;
+                    download_versionedapp = 1;
+                    app_name = optarg;
+                    break;
                 case 'x':
                     is_broadband = 1;
                     download_versionedapp = 1;
