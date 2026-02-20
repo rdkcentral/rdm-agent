b51152f14520ac364caaa93a9454d5b53f95e55f
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-02-20 15:48:24 +0530

    Change rdmRbusSetDownloadStatus parameter type

--- include/rdm_rbus.h
+++ include/rdm_rbus.h
@@ -32,5 +32,5 @@ enum rfcType
 INT32 rdmRbusInit(VOID **pRDMRbusHandle, INT8* rbusName);
 INT32 rdmRbusGetRfc(VOID *plRDMRbusHandle, INT8* rdmRFCName, VOID *pValue);
 VOID  rdmRbusUnInit(VOID *pRDMbusHandle);
-INT32 rdmRbusSetDownloadStatus(rbusHandle_t handle, bool value);
+INT32 rdmRbusSetDownloadStatus(VOID *handle, bool value);
 #endif //_RDM_RBUS_H_
