280005449a9d449ccdb629297c289930ee5715a3
Aryan Y <aryan_duttay@comcast.com>
2026-07-23 10:05:51 +0000

    change in log level to avoid log flood

--- src/rdm_download.c
+++ src/rdm_download.c
@@ -62,12 +62,12 @@ static INT32 rdmDownloadCheckFs(RDMAPPDetails *pRdmAppDet)
 
         fs_status = emptyFolder(pRdmAppDet->app_dwnl_path);
         if(fs_status) {
-            RDMError("Failed to empty the folder: %s\n", pRdmAppDet->app_dwnl_path);
+            RDMWarn("Failed to empty the folder: %s\n", pRdmAppDet->app_dwnl_path);
         }
 
         fs_status = emptyFolder(pRdmAppDet->app_home);
         if(fs_status) {
-            RDMError("Failed to empty the folder: %s\n", pRdmAppDet->app_home);
+            RDMWarn("Failed to empty the folder: %s\n", pRdmAppDet->app_home);
         }
 
         fs_freespace = getFreeSpace(pRdmAppDet->app_mnt);
